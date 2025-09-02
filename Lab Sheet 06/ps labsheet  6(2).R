30bd21a0dd74ce7ba3ffd6e3ea1a454127d65632
W.G.H.H. hansani <it24103762@my.sliit.lk>
2025-09-02 10:28:14 +0530

    Add files via upload

---
+++ Lab Sheet 06/ps labsheet  6(2).R
@@ -0,0 +1,19 @@
+setwd("C:/Users/IT24103762/Desktop/IT24103762")
+
+
+#part 2
+1-pbinom(46,50,0.85,lower.tail=TRUE)
+
+#Q2
+
+#part 1
+#X=the number of customer calls received by the call center in one hour.
+
+#part 2
+#The distribution of X is a Poisson distribution with mean 12.
+
+#part 3
+x<-15
+lambda<-12
+
+dpois(x,lambda)
